877e535fb1339f1c3b04b6338aadc13317c36647
Mahbubur Rahman <rifat0153@gmail.com>
2022-02-10 00:06:11 +0000

    VIew init

--- app/src/main/java/com/example/wallpaperapp/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.wallpaperapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.wallpaperapp.model.util.kSpacing
 import com.example.wallpaperapp.ui.theme.WallpaperAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,10 +28,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val isVisible = remember { mutableStateOf(false) }
+
             WallpaperAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.padding(kSpacing.dp)) {
+
+                        AnimatedVisibility(visible = isVisible.value,
+                        enter = EnterTransition.None,
+
+                            ) {
+                            Greeting("Android")
+                        }
+
+
+
+
+
+                        TextButton(onClick = {
+                            isVisible.value = !isVisible.value
+                        })
+                        {
+                            Text(text = "Show")
+                        }
+
+
+                       if(isVisible.value) Text(text = "Kire", style = TextStyle(fontSize = 24.sp))
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/wallpaperapp/model/util/constants.kt
+++ app/src/main/java/com/example/wallpaperapp/model/util/constants.kt
@@ -6,3 +6,5 @@ object Constants {
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
     const val UNSPLASH_REMOTE_KEYS_TABLE = "unsplash_remote_keys_table"
 }
+
+val kSpacing = 8;
